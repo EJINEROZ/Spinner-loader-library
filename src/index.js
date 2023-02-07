fb9a990d3267cc54a8eb3c0dbd4f6b83d8e22558
Ejiro Uweghrepha <ejineroz@Ejiros-MacBook-Air.local>
2023-02-07 11:57:26 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+import Spinner from "./Spinner";
+
+export const Spine = ({}) => {
+  return (
+    <div className={styles.pos}>
+      <Spinner />
+    </div>
+  );
 }
